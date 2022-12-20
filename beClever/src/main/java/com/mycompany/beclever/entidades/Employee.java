b2c026dbe857c590be3c42a7362b2c86758c058f
GastonSKL <gastonlskamlec@gmail.com>
2022-12-20 11:24:26 -0300

    Primer commit
    Creacion de proyecto en JAVA + creacion de clase Employee con sus respectivos campos y metodos de entidad

---
+++ beClever/src/main/java/com/mycompany/beclever/entidades/Employee.java
@@ -0,0 +1,119 @@
+package com.mycompany.beclever.entidades;
+
+import java.time.LocalDate;
+import java.util.Objects;
+
+public class Employee {
+    
+    private int idEmployee;
+    private String name;
+    private LocalDate date;
+    private boolean registerType;
+    private String businessLocation;
+
+    public Employee() {
+    }
+
+    public Employee(String name,LocalDate date, boolean registerType, String businessLocation) {
+        this.name = name;
+        this.date = date;
+        this.registerType = registerType;
+        this.businessLocation = businessLocation;
+    }
+
+    public Employee(int idEmployee,String name, LocalDate date, boolean registerType, String businessLocation) {
+        this.idEmployee = idEmployee;
+        this.name = name;
+        this.date = date;
+        this.registerType = registerType;
+        this.businessLocation = businessLocation;
+    }
+
+    public int getIdEmployee() {
+        return idEmployee;
+    }
+
+    public void setIdEmployee(int idEmployee) {
+        this.idEmployee = idEmployee;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public boolean isRegisterType() {
+        return registerType;
+    }
+
+    public void setRegisterType(boolean registerType) {
+        this.registerType = registerType;
+    }
+
+    public String getBusinessLocation() {
+        return businessLocation;
+    }
+
+    public void setBusinessLocation(String businessLocation) {
+        this.businessLocation = businessLocation;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 29 * hash + this.idEmployee;
+        hash = 29 * hash + Objects.hashCode(this.name);
+        hash = 29 * hash + Objects.hashCode(this.date);
+        hash = 29 * hash + (this.registerType ? 1 : 0);
+        hash = 29 * hash + Objects.hashCode(this.businessLocation);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Employee other = (Employee) obj;
+        if (this.idEmployee != other.idEmployee) {
+            return false;
+        }
+        if (this.registerType != other.registerType) {
+            return false;
+        }
+        if (!Objects.equals(this.name, other.name)) {
+            return false;
+        }
+        if (!Objects.equals(this.businessLocation, other.businessLocation)) {
+            return false;
+        }
+        return Objects.equals(this.date, other.date);
+    }
+
+    @Override
+    public String toString() {
+        return "Id del empleado: " + idEmployee + ", Nombre :" + name + ", Fecha de registro: " + date + ", Tipo de registro: " + registerType + ", Localizacion: " + businessLocation;
+    }
+    
+
+    
+    
+    
+}
